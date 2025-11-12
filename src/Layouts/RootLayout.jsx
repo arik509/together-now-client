cab42c5ae5c3e6c7a0bbe9ff6e6567b48e1a872b
arik509 <sabirhossainarik34@gmail.com>
2025-11-12 22:14:55 +0600

    Added tailwind and daisyui

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
+import { Outlet } from 'react-router';
 
 const RootLayout = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import RootLayout from "./Layouts/RootLayout.jsx";
+import Home from "./Pages/Home.jsx";
 
 const router = createBrowserRouter([
   {
